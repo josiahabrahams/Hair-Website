c4e8945c2db1e33beee8d944629d586ed5dc3ae7
josiahabrahams <josiahabrahams1@gmail.com>
2023-10-09 11:42:44 +0200

    adding new sections and some dynamic feautures

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,47 @@
 import { useState } from 'react'
 import {AboutSection} from './AboutSection/AboutSection.tsx'
+import {ContactUsSection} from './ContactUsSection/ContactUsSection.tsx'
+import {ServicesSection} from './ServicesSection/ServicesSection.tsx'
 import './App.css'
 import {Button, CssBaseline } from '@mui/material'
 
 export const  App = () => {
-
-
+  const [showAboutSection , setShowAboutSection] = useState<boolean>(true)
+  const [showContactSection , setShowContactSection] = useState<boolean>(false)
+  const [showServicesSection , setShowServicesSection] = useState<boolean>(false)
+  
+  const showAboutSec = () =>{
+    setShowAboutSection(true)
+    setShowContactSection(false)
+    setShowServicesSection(false)
+  }
+  const showContactSec = () =>{
+    setShowContactSection(true)
+    setShowAboutSection(false)
+    setShowServicesSection(false)
+  }
+  const showServicesSec = () =>{
+    setShowServicesSection(true)
+    setShowAboutSection(false)
+    setShowContactSection(false)
+    
+  }
+  
+  
   return (
     <>
        <CssBaseline/>
        <header>
          <h1>Hair Dresser</h1>
          <div>
-           <Button style={{color: 'white'}}>About</Button>
-           <Button style={{color: 'white'}}>Services</Button>
-           <Button style={{color: 'white'}}>Contact us</Button>
+           <Button style={{color: 'white'}} onClick={showAboutSec}>About</Button>
+           <Button style={{color: 'white'}} onClick={showServicesSec}>Services</Button>
+           <Button style={{color: 'white'}} onClick={showContactSec}>Contact us</Button>
          </div>
        </header>
-       <AboutSection/>
+      {showAboutSection && <AboutSection/>}
+      {showContactSection && <ContactUsSection/>}
+      {showServicesSection && <ServicesSection/>}
        <footer>
        <div>
         Photo by <a href="https://unsplash.com/@albertobobbera?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Alberto Bobbera</a> on <a href="https://unsplash.com/photos/bHWit6pkuAk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
